9e0546ab77c654c0977fef8874864fc1a829a2a6
RashakDude <rajatagarwal686@gmail.com>
2021-04-25 02:53:11 +0530

    finish

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './App.css';
 import QuoteBox from './QuoteBox';
 
-function App() {
-  return (
-    <div id="app" className="gray">
-      <QuoteBox/>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = ({
+      color : "purple"
+    });
+    this.handleColorChange = this.handleColorChange.bind(this);
+  }
+
+  // Change the color of the app
+  handleColorChange(color) {
+    this.setState({
+      color: color
+    });
+  }
+
+  render(){
+    return (
+      <div id="app" className={this.state.color}>
+        <QuoteBox onChangeAppColor={this.handleColorChange} color={this.state.color}/>
+      </div>
+    );
+  } 
 }
 
 export default App;
--- src/QuoteBox.jsx
+++ src/QuoteBox.jsx
@@ -1,19 +1,48 @@
-import { render } from '@testing-library/react';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class QuoteBox extends React.Component{
+
     constructor(props){
         super(props);
         this.state = {
             quoteText : "",
             quoteAuthor : "",
             curColor : this.props.color,
-            tweetURL : "https://twitter.com/intent/tweet/?text="
         }
+
+        this.getNewQuote();
+
+        // Bind handles and functions
+        this.getNewQuote = this.getNewQuote.bind(this);
+        this.handleNewQuote = this.handleNewQuote.bind(this);
     }
+
+    // Get a quote and author from an API and set status
+    getNewQuote() {
+        const app = this;
+        fetch('https://api.quotable.io/random')
+        .then((response) => response.json())
+        .then((data) => {
+          app.setState({
+            quoteText: data.content,
+            quoteAuthor: data.author
+          });
+        });
+    }
+
+    // Handles the new quote button click
+    handleNewQuote() {
+        const colors = ["gray", "blue", "purple", "red", "orange", "green"];
+        let color = colors[Math.floor(Math.random()*colors.length)];
+        while(color === this.state.curColor) color = colors[Math.floor(Math.random()*colors.length)];
+        this.setState({
+            curColor: color
+        });
+        this.props.onChangeAppColor(color);
+        this.getNewQuote();
+    }
+
 
     render(){
         return (
@@ -22,8 +51,7 @@ class QuoteBox extends React.Component{
                     <div id="text">{ this.state.quoteText }</div>
                     <div id="author">{ this.state.quoteAuthor }</div>
                 </div>
-                <button id="new-quote">Get New Quote</button>
-                <a href={ this.state.tweetURL } id="tweet-quote"><FontAwesomeIcon icon={['fab', 'twitter-fa']} /> Tweet Quote</a>
+                <button onClick={this.handleNewQuote} id="new-quote">Get New Quote</button>
             </main>
         );
     }
